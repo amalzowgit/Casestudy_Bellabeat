85c5f510e375633370e488b6fe5665e59273c21e
Angela <Angelamalzow@gmail.com>
2022-03-21 13:17:27 -0500

    ggplot on daily HR
    with color palette color per unique session id

--- Scripts/fitbit_heartrate_seconds_visuals.R
+++ Scripts/fitbit_heartrate_seconds_visuals.R
@@ -1,6 +1,6 @@
 # load libraries
 library(tidyverse)
-library(lubridate)
+library(viridis)
 
 # set theme
 theme_set(theme_classic())
@@ -8,11 +8,20 @@ theme_set(theme_classic())
 # view data
 heartrates_vis1 <- read_csv("/Documents/R/Casestudy_Bellabeat/cleaned_data/fitbit_hr_min_max_per_id_date_2.csv")
 View(heartrates_vis1)
-
 str(heartrates_vis1)
 colnames(heartrates_vis1)
 
-heartrates_vis2 <- heartrates_vis1 %>% 
-  ggplot(aes(x = date, y = meanhr)) +
-  geom_point()
+#hrviz2 <- heartrates_vis1 %>% 
+  #group_by(id) %>% 
+  #dplyr::mutate(id = cur_group_id())
+
+#View(hrviz2)
+
+heartrates_vis1 %>% 
+  ggplot(aes(x = date, y = meanhr, color = id)) +
+  geom_point() +
+  scale_x_date(date_labels = "%b %d", date_breaks = "1 day") +
+  theme(legend.position = "right", axis.text.x = element_text(angle = 90))
+
+
 
